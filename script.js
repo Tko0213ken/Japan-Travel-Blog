0ed7c59793ab9bd51474a48d6ba4f5066b71fbe2
Takehiro Yamauchi <takehiri@gmail.com>
2025-02-02 15:29:51 +0900

    ハンバーガーメニュー文字サイズ修正 ポピュラーポストランキング番号追加 フッター修正（ナビゲーション削除等）

--- script.js
+++ script.js
@@ -15,23 +15,67 @@ closeElements.forEach((element) => {
   });
 });
 
-// function updateHeights() {
-//   const ppNo1Element = document.querySelector("#base-imgHight");
-//   const ppNo1Hight = ppNo1Element.getBoundingClientRect().height;
-//   const rowGap = 0;
-//   const ppNo2to4 = ppNo1Hight - rowGap * 2;
-//   document.documentElement.style.setProperty("--img-height", `${ppNo2to4}px`);
-//   console.log(`No1の高さ: ${ppNo1Hight}, No2~4の高さ: ${ppNo2to4}`);
-// }
-// document.addEventListener("DOMContentLoaded", updateHeights);
-// window.addEventListener("resize", updateHeights);
-
 // 画面幅1024px時、ヘッダー
+// document.addEventListener("DOMContentLoaded", () => {
+//   const hamburger = document.getElementById("js-btnHamburger");
+//   const gnav = document.querySelector(".gnav");
+//   const overlay = document.querySelector(".gnav-overlay");
+//   const body = document.body;
+
+//   function toggleMenu() {
+//     if (gnav.classList.contains("_open")) {
+//       gnav.classList.remove("_open");
+//       overlay.classList.remove("_open");
+//       body.classList.remove("no-scroll");
+//       hamburger.classList.remove("_open");
+//     } else {
+//       gnav.classList.add("_open");
+//       overlay.classList.add("_open");
+//       body.classList.add("no-scroll");
+//       hamburger.classList.add("_open");
+//     }
+//   }
+//   hamburger.addEventListener("click", toggleMenu);
+//   overlay.addEventListener("click", toggleMenu);
+
+//   // 画面幅1024px時、ヘッダーのアコーディオン開閉処理
+//   const accordionToggles = document.querySelectorAll(
+//     ".gnav__link--has-dropdown"
+//   );
+
+//   accordionToggles.forEach((toggle) => {
+//     toggle.addEventListener("click", (e) => {
+//       // e.preventDefault();
+
+//       const dropdown = toggle.querySelector(".dropdown");
+
+//       if (dropdown.classList.contains("_open")) {
+//         dropdown.classList.remove("_open");
+//       } else {
+//         document.querySelectorAll(".dropdown._open").forEach((openDropdown) => {
+//           openDropdown.classList.remove("_open");
+//         });
+
+//         dropdown.classList.add("_open");
+//       }
+//     });
+//   });
+// });
+
 document.addEventListener("DOMContentLoaded", () => {
   const hamburger = document.getElementById("js-btnHamburger");
   const gnav = document.querySelector(".gnav");
   const overlay = document.querySelector(".gnav-overlay");
   const body = document.body;
+  const accordionToggles = document.querySelectorAll(
+    ".gnav__link--has-dropdown"
+  );
+
+  function closeAllDropdowns() {
+    document.querySelectorAll(".dropdown._open").forEach((openDropdown) => {
+      openDropdown.classList.remove("_open");
+    });
+  }
 
   function toggleMenu() {
     if (gnav.classList.contains("_open")) {
@@ -39,6 +83,8 @@ document.addEventListener("DOMContentLoaded", () => {
       overlay.classList.remove("_open");
       body.classList.remove("no-scroll");
       hamburger.classList.remove("_open");
+
+      closeAllDropdowns();
     } else {
       gnav.classList.add("_open");
       overlay.classList.add("_open");
@@ -46,27 +92,18 @@ document.addEventListener("DOMContentLoaded", () => {
       hamburger.classList.add("_open");
     }
   }
+
   hamburger.addEventListener("click", toggleMenu);
   overlay.addEventListener("click", toggleMenu);
 
-  // 画面幅1024px時、ヘッダーのアコーディオン開閉処理
-  const accordionToggles = document.querySelectorAll(
-    ".gnav__link--has-dropdown"
-  );
-
   accordionToggles.forEach((toggle) => {
     toggle.addEventListener("click", (e) => {
-      // e.preventDefault();
-
       const dropdown = toggle.querySelector(".dropdown");
 
       if (dropdown.classList.contains("_open")) {
         dropdown.classList.remove("_open");
       } else {
-        document.querySelectorAll(".dropdown._open").forEach((openDropdown) => {
-          openDropdown.classList.remove("_open");
-        });
-
+        closeAllDropdowns();
         dropdown.classList.add("_open");
       }
     });
